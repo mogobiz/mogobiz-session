e5c35e637e3d4551e9606e77ef6134a00bfd49d4
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-11-06 16:47:14 +0100

    no message

--- src/main/scala/com/mogobiz/session/Settings.scala
+++ src/main/scala/com/mogobiz/session/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
 
     val Folder = new File(config getString "session.folder")
 
-    val EsIndex =config getString "session.esindex"
+    val EsIndex = config getString "session.esindex"
 
     val CookieName = config getString "session.cookie.name"
 
@@ -21,9 +21,10 @@ object Settings {
     val RememberCookieName = config getString "session.remember.cookie.name"
 
     val RememberCookieMaxAge = config getLong "session.remember.cookie.maxage"
+
+    require(Secret.nonEmpty, "session.secret must be non-empty")
+    require(CookieName.nonEmpty, "session.cookie.name must be non-empty")
+    require(RememberCookieName.nonEmpty, "session.remember.cookie.name must be non-empty")
   }
 
-  require(Session.Secret.nonEmpty, "session.secret must be non-empty")
-  require(Session.CookieName.nonEmpty, "session.cookie.name must be non-empty")
-  require(Session.RememberCookieName.nonEmpty, "session.remember.cookie.name must be non-empty")
 }
