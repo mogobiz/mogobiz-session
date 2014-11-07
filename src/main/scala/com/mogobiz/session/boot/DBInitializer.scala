3c8c7f746a4e7ce67620db9edcfcfa60040a3974
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-11-07 14:28:38 +0100

    handle error message

--- src/main/scala/com/mogobiz/session/boot/DBInitializer.scala
+++ src/main/scala/com/mogobiz/session/boot/DBInitializer.scala
@@ -5,6 +5,7 @@ import com.mogobiz.session.config.Settings
 import com.mogobiz.session.es.Mapping
 import com.sksamuel.elastic4s.ElasticDsl._
 import org.elasticsearch.indices.IndexAlreadyExistsException
+import org.elasticsearch.transport.RemoteTransportException
 
 object DBInitializer {
   def apply(): Unit = try {
@@ -12,7 +13,7 @@ object DBInitializer {
     Mapping.set()
     fillDB()
   } catch {
-    case e: IndexAlreadyExistsException =>
+    case e: RemoteTransportException if e.getCause().isInstanceOf[IndexAlreadyExistsException] =>
       println(s"Index ${Settings.Session.EsIndex} was not created because it already exists.")
     case e: Throwable => e.printStackTrace()
   }
