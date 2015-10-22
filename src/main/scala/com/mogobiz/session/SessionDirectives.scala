6eb3bc3ac25b52faf3c239063a81f9a40a48cb7b
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-10-22 18:20:10 +0200

    minor

--- src/main/scala/com/mogobiz/session/SessionDirectives.scala
+++ src/main/scala/com/mogobiz/session/SessionDirectives.scala
@@ -66,7 +66,6 @@ trait SessionDirectives {
     Session(backend.load(cookie.content).map(_.data).getOrElse(Map.empty[String, Any]), cookie.expires, cookie.maxAge, cookie.domain, cookie.path, cookie.secure, cookie.httpOnly, cookie.extension)
 
   implicit def sessionToCookie(session: Session): HttpCookie = {
-    println("storing cookie" + session.id)
     val res = HttpCookie(Settings.Session.CookieName, backend.store(session), session.expires, session.maxAge, session.domain, session.path, session.secure, session.httpOnly, session.extension)
     res
   }
